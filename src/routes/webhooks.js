4211c43d9cda924252656335f42097de42e18e77
Tushar Khatri <tusharkhatri365@gmail.com>
2026-04-01 18:47:00 +0530

    fixing webhook for shopfiy

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -1,13 +1,3 @@
 import express from 'express';
 import { handleShopifyOrderCreate, handleTwilioReply, handleRazorpayWebhook } from '../controllers/webhookController.js';
 
-const router = express.Router();
-
-router.post('/shopify/orders/create', handleShopifyOrderCreate); // Legacy
-router.post('/shopify/order-created', handleShopifyOrderCreate); // Standardized
-router.post('/whatsapp', handleTwilioReply);
-
-// Razorpay Partial Payment Callbacks (supports GET for demo redirects)
-router.all('/payment/razorpay', handleRazorpayWebhook);
-
-export default router;
